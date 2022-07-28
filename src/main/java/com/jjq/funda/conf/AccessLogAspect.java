66c563d141d7a51e0bebf62c4e09c4ae2107644d
jiangjunqing <2916147177@qq.com>
2022-07-28 23:56:32 +0800

    http客户端bean配置

--- src/main/java/com/jjq/funda/conf/AccessLogAspect.java
+++ src/main/java/com/jjq/funda/conf/AccessLogAspect.java
@@ -1,5 +1,6 @@
 package com.jjq.funda.conf;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 /**
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
  * @date 2022/7/27
  * @desc 系统访问日志切面
  */
+@Slf4j
 @Component
 public class AccessLogAspect {
 
