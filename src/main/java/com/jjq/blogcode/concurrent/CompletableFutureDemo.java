8e2ff135dc184f7f14e8982e6d971a038238c714
jiangjunqing <2916147177@qq.com>
2022-08-03 23:51:28 +0800

    CompletableFuture例子

--- src/main/java/com/jjq/blogcode/concurrent/CompletableFutureDemo.java
+++ src/main/java/com/jjq/blogcode/concurrent/CompletableFutureDemo.java
@@ -3,6 +3,7 @@ package com.jjq.blogcode.concurrent;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.*;
+import java.util.stream.IntStream;
 
 /**
  * @author qingshan1993
@@ -37,6 +38,38 @@ public class CompletableFutureDemo {
         });
         String result = completableFuture.get();
         System.out.println("result: " + result);
+    }
+
+    @Test
+    public void test3() throws InterruptedException {
+        Thread thread1 = new Thread(() -> IntStream.range(0, 10).forEach(System.out::println));
+        Thread thread2 = new Thread(() -> {
+            try {
+                thread1.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            IntStream.range(10, 20).forEach(System.out::println);
+        });
+        thread1.join();
+        thread1.start();
+        thread2.start();
+    }
+
+    @Test
+    public void test4() {
+        CompletableFuture
+                .runAsync(() -> IntStream.range(0, 10).forEach(System.out::println))
+                .thenRunAsync(() -> IntStream.range(10, 20).forEach(System.out::println));
+    }
+
+    @Test
+    public void test5() {
+        //这是错误的写法，执行后，什么也不会发生
+        CompletableFuture<Void> completableFuture = new CompletableFuture<>();
+        completableFuture
+                .thenRunAsync(() -> IntStream.range(0, 10).forEach(System.out::println))
+                .thenRunAsync(() -> IntStream.range(10, 20).forEach(System.out::println));
     }
 
 
